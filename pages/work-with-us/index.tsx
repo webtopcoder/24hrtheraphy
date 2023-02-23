12b87a7265a0a563cb283aefd64422e515bda84f
Samrat Neupane <samratneupane215@gmail.com>
2023-02-23 11:54:31 +0545

    Fixed worked with us flow (#6)
    Fixed ref issues in work with us page
    Seperated steps to custom hook
    Removed classes and used antd in work with us steps

--- pages/work-with-us/index.tsx
+++ pages/work-with-us/index.tsx
@@ -2,164 +2,18 @@ import { IUIConfig } from "src/interfaces";
 import "./index.less";
 import { connect } from "react-redux";
 import Head from "next/head";
-import {
-  Row,
-  Col,
-  Button,
-  Modal,
-  Steps,
-  message,
-  Radio,
-  Space,
-  Input,
-} from "antd";
+import { Row, Col, Modal } from "antd";
 import Link from "next/link";
-import { useRef, useState } from "react";
+import { useState } from "react";
+import WorkWithUsSteps from "@components/auth/register/work-with-us-steps";
 
 interface IProps {
   ui: IUIConfig;
   settings?: any;
 }
 
-const RegisterForm = () => {
-  return <p>This is a register form.</p>;
-};
-
-function Homepage({ settings, ui }: IProps) {
-  const otherRef = useRef([]);
-  otherRef.current = [];
-
-  const addToRefs: (el) => void = (el) => {
-    if (el && !otherRef.current.includes(el)) {
-      otherRef.current.push(el);
-    }
-  };
-
-  const inputRef = useRef([]);
-  inputRef.current = [];
-
-  const addToInputRefs: (el) => void = (el) => {
-    if (el && !inputRef.current.includes(el)) {
-      inputRef.current.push(el);
-    }
-  };
-
+function WorkWithUs({ settings, ui }: IProps) {
   const [modalOpen, setModalOpen] = useState(false);
-  const steps = [
-    {
-      title: "License Type",
-      question: "Select your license type to get started.",
-      formName: "licenseType",
-      options: [
-        "Psychologist",
-        "Marriage and Family Therapist",
-        "Clinical Social Worker",
-        "Child Therapist",
-        "Mental Health Counselor",
-        "Professional Counselor",
-      ],
-    },
-    {
-      title: "Question",
-      question: "Where do you currently practice your license?",
-      formName: "secondQuestion",
-      options: [
-        "Clinical Practice",
-        "Online Therapy Platforms",
-        "Private Practice",
-        "Community Health Center",
-        "Practice on Academic or Research Setting",
-        "Other",
-      ],
-    },
-    {
-      title: "Question",
-      question: "Why do you want to join 24 hour therapy?",
-      formName: "thirdQuestion",
-      options: [
-        "Interested in Online Therapy",
-        "To grow my private practice",
-        "To work as a full time",
-        "To work as a part time",
-        "Other",
-      ],
-    },
-    {
-      title: "Question",
-      question:
-        "How much time do you intend to be available for 24 hour therapy clients?",
-      formName: "fourthQuestion",
-      options: [
-        "Be flexible in time",
-        "Up to 5 hours a week",
-        "5 to 10 hours a week",
-        "10 to 20 hours a week",
-        "Other than 30 hours a week",
-        "Not decided yet",
-      ],
-    },
-    {
-      title: "Question",
-      question: "How did you first hear about 24 hour Therapy?",
-      formName: "finalQuestion",
-      options: [
-        "Internet Research",
-        "Social Media",
-        "Friend or colleague",
-        "Email Outreach",
-        "Other",
-      ],
-    },
-    {
-      title: "Form",
-      content: <RegisterForm />,
-    },
-  ];
-
-  const items = steps.map((item) => ({
-    key: item.title,
-    title: item.title,
-  }));
-
-  const [current, setCurrent] = useState(0);
-  const next = () => {
-    setCurrent(current + 1);
-  };
-
-  const prev = () => {
-    setCurrent(current - 1);
-  };
-
-  const [radioValue, setRadioValue] = useState("");
-  const initialFormValues = [
-    {
-      answer: "",
-    },
-  ];
-  const [formValues, setFormValues] = useState(initialFormValues);
-  const onChange = (e) => {
-    console.log("radio checked", e.target.value);
-    setRadioValue(e.target.value);
-  };
-
-  const handleNextClick = () => {
-    for (let i = 0; i <= 5; i++) {
-      const checkedValue = otherRef?.current[i]?.props?.value;
-      const inputValue = inputRef?.current[i]?.input?.value;
-      if (otherRef?.current[i]?.input?.checked) {
-        if (checkedValue === "Other") {
-          setFormValues((prevState) => [...prevState, { answer: inputValue }]);
-        } else {
-          setFormValues((prevState) => [
-            ...prevState,
-            { answer: checkedValue },
-          ]);
-        }
-      }
-    }
-    setRadioValue("");
-    next();
-  };
 
   return (
     <>
@@ -314,7 +168,7 @@ function Homepage({ settings, ui }: IProps) {
       </div>
       <Modal
         centered
-        width="1200px"
+        width="1000px"
         open={modalOpen}
         onCancel={() => setModalOpen(false)}
         okButtonProps={{
@@ -323,96 +177,9 @@ function Homepage({ settings, ui }: IProps) {
         cancelButtonProps={{
           hidden: true,
         }}
+        className="modal-content-container"
       >
-        <div className="modal-content-container">
-          <Steps
-            style={{
-              marginTop: "3rem",
-            }}
-            current={current}
-            items={items}
-          />
-
-          <div
-            style={{
-              marginBottom: "3rem",
-              marginTop: "3rem",
-            }}
-          >
-            {steps[current]?.question ? (
-              <h5>Q. {steps[current]?.question}</h5>
-            ) : (
-              ""
-            )}
-
-            {steps[current]?.content}
-            <Radio.Group onChange={onChange} value={radioValue}>
-              <Space direction="vertical">
-                {steps[current]?.options?.map((item, index) => (
-                  <div className="question-container" key={index}>
-                    {item === "Other" ? (
-                      <Radio value={item} ref={addToRefs}>
-                        {item}
-                        <Input
-                          ref={addToInputRefs}
-                          style={{
-                            width: 100,
-                            marginLeft: 10,
-                          }}
-                        />
-                      </Radio>
-                    ) : (
-                      <Radio value={item} ref={addToRefs}>
-                        {item}
-                      </Radio>
-                    )}
-                  </div>
-                ))}
-              </Space>
-            </Radio.Group>
-          </div>
-          <div
-            style={{
-              marginTop: 24,
-            }}
-          >
-            {current < steps.length - 1 && (
-              <Button
-                className="modal-cta-button"
-                type="primary"
-                style={{ height: `50px` }}
-                onClick={handleNextClick}
-              >
-                Next
-              </Button>
-            )}
-            {current === steps.length - 1 && (
-              <Button
-                className="modal-cta-button"
-                type="primary"
-                style={{ height: `50px` }}
-                onClick={() => {
-                  message.success("Processing complete!");
-                  formValues.shift();
-                  console.log(formValues);
-                }}
-              >
-                Done
-              </Button>
-            )}
-            {current > 0 && (
-              <Button
-                className="modal-cta-button"
-                style={{
-                  height: `50px`,
-                }}
-                onClick={() => prev()}
-              >
-                Previous
-              </Button>
-            )}
-          </div>
-        </div>
+        <WorkWithUsSteps />
       </Modal>
     </>
   );
@@ -420,7 +187,7 @@ function Homepage({ settings, ui }: IProps) {
 
 const mapStateToProps = (state) => ({
   ui: { ...state.ui },
-  settings: state.settings,
+  ...state.auth.performerRegister,
 });
 
-export default connect(mapStateToProps)(Homepage);
+export default connect(mapStateToProps)(WorkWithUs);
